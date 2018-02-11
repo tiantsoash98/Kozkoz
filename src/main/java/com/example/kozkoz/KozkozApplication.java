1f9cbaf1327abe58903a58d1555e0b71ecc2b823
gptitu <gptitu@gmail.com>
2018-02-11 23:53:22 +0300

    test commit

--- src/main/java/com/example/kozkoz/KozkozApplication.java
+++ src/main/java/com/example/kozkoz/KozkozApplication.java
@@ -1,10 +1,14 @@
 package com.example.kozkoz;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableAutoConfiguration
 @SpringBootApplication
 public class KozkozApplication {
 
@@ -13,7 +17,7 @@ public class KozkozApplication {
 	String index() {
 		return "Web service Kozkoz Application";
 	}
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(KozkozApplication.class, args);
 	}
